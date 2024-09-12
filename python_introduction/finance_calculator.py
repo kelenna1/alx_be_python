b71bb7fb25ec23c2859bdddd8ce8aebc59a74444
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-09-12 12:28:29 +0100

    checker wahala

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,8 +3,5 @@ expenses = int(input("Enter your total monthly expenses:"))
  
 savings = income - expenses
 annual_savings = int(savings * 12 + (savings * 12 * 0.05))
-if expenses >= income:
-    print("You made no savings this month")
-else:
-    print(f"Your monthly savings are ${savings}.")
-    print(f"Projected savings after one year, with interest, is: ${annual_savings}")
+print(f"Your monthly savings are ${savings}.")
+print(f"Projected savings after one year, with interest, is: ${annual_savings}")

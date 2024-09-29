1aaff97f1d8cb3b6e0b31f823094131e43249a81
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-09-29 22:00:39 +0100

    modules and packages

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                print("cannot divide by zero")
+            else:
+                return num1 / num2
+        case _:
+            return "Error! invalid input"
+        

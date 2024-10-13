ca076f4e8e1acc3450fe34ce86bf76701ac89e9b
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-10-13 18:59:48 +0100

    changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,11 +1,11 @@
 
 
 class Book:
-    def __init__(self, title, author) ->:
+    def __init__(self, title, author):
         self.title =title
         self.author = author
 
-    def __str__(self) ->:
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
